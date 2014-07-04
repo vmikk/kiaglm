1c8c21f6018c37d19f9f13a1971b6d2515eafcc2
Vladimir Mikryukov <vmikryukov@gmail.com>
2014-07-04 11:25:16 +0600

    initial commit, v.0.1

---
+++ server.R
@@ -0,0 +1,297 @@
+
+
+# library(shiny)
+# setwd("c:/Users/mikryukov_vs/Dropbox/R/")
+# runApp("KIA - GLM")
+
+
+library(car)
+
+source("df.summary.r")    # str.df function  (==  ezPrecis from ez-package)
+
+
+# transform data.frame (used for confinit)
+df.ci <- function(x) {
+  res <- data.frame(Variable = rownames(x), x)
+  rownames(res) <- NULL
+  colnames(res) <- c("Variable", "LCI", "UCI")
+  return(res)
+}
+
+
+shinyServer(function(input, output) {
+
+
+#####################
+##################### # load data
+#####################
+
+parseUserData <- reactive({        # based on code by Huidong Tian  - Flexible upload data
+    inFile <- input$dataFile
+    if(!is.null(inFile)) {
+      # Determine document format;
+      ptn <- "\\.[[:alnum:]]{1,5}$"
+      suf <- tolower(regmatches(inFile$name, regexpr(ptn, inFile$name)))
+      
+      # Options for Excel documents;
+      if(suf %in% c('.xls', '.xlsx')) {
+        wb <- loadWorkbook(inFile$datapath)
+        sheets <- getSheets(wb)
+        dat <- readWorksheet(wb, sheets[1])
+          ind <- sapply(dat, is.character)         # which columns are "chr"
+          dat[ind] <- lapply(dat[ind], factor)     # convert them to factor
+        return(dat)
+      } 
+      
+      # Options for txt documents;
+      if(suf %in% '.txt') {
+        read.table(file = inFile$datapath, header=TRUE, sep="\t")
+      }
+
+    } else {return(NULL)}
+})
+  
+
+userData <- reactive({ parseUserData() })
+
+
+
+# Maximal number of observations to exclude
+output$excl.n <- renderUI({
+  if(!is.null( userData() )){
+    n <- nrow( userData() )
+    max.n <- ceiling(90*n / 100)         # approximately 10% of data should be always preserved
+    current.excl <- ceiling(10*n / 100)  # by default exclude 10% of data
+    return(
+    sliderInput("n.excl", label = "Observations to exclude:",
+          min = 1, max = max.n, value = current.excl, step=1)
+    )
+  } else {
+    return(NULL)
+  }
+})
+
+
+
+# Data preview
+output$data.descr <- renderPrint(
+  if(!is.null( userData() )){
+    str.df( userData() )
+  } else {
+    print(data.frame(Warning="Please upload your data."))
+  }
+)
+
+
+#####################
+##################### # Build the model
+#####################
+
+
+formulaText <- reactive({
+  ff <- try( as.formula(input$ff) )
+  if( any(class(ff) == "try-error")) { return(NULL) }
+  if( any(class(ff) != "try-error")) { return(ff) }
+})
+
+
+# build model
+mod <- reactive({
+  if(input$ff == "") { return(NULL) } 
+
+  if(formulaText() != "") {
+  if(input$family == "binomial")          { fit <- try( glm(formulaText(), data=userData(), family=binomial())) }
+  if(input$family == "gaussian")          { fit <- try( glm(formulaText(), data=userData(), family=gaussian())) }
+  if(input$family == "Gamma")             { fit <- try( glm(formulaText(), data=userData(), family=Gamma())) }
+  if(input$family == "inverse.gaussian")  { fit <- try( glm(formulaText(), data=userData(), family=inverse.gaussian())) }
+  if(input$family == "poisson")           { fit <- try( glm(formulaText(), data=userData(), family=poisson())) }
+  if(input$family == "quasibinomial")     { fit <- try( glm(formulaText(), data=userData(), family=quasibinomial())) }
+  if(input$family == "quasipoisson")      { fit <- try( glm(formulaText(), data=userData(), family=quasipoisson())) }
+  
+  if( any(class(fit) != "try-error")) { return(fit) }
+  if( any(class(fit) != "try-error")) { return(NULL) }
+  }
+})
+
+# print coefficients
+output$regTab <- renderPrint({
+  if(!is.null(input$ff)){
+    summary(mod())
+  } else {
+    print(data.frame(Warning="Please select Model Parameters."))
+  }
+})
+
+
+# Diagnostic plots
+output$diagnost.1 <- renderPlot({
+  if(!is.null(mod())){ plot(mod(), which=1) }
+})
+output$diagnost.2 <- renderPlot({
+  if(!is.null(mod())){ plot(mod(), which=2) }
+})
+output$diagnost.3 <- renderPlot({
+  if(!is.null(mod())){ plot(mod(), which=3) }
+})
+output$diagnost.4 <- renderPlot({
+  if(!is.null(mod())){ plot(mod(), which=4) }
+})
+
+
+
+######################################
+######################################  Exclusion
+######################################
+
+# Plot for which coefficient we should show?
+output$show <- renderUI({
+  if(!is.null( mod() )){
+    terms <- names(coefficients(mod()))    # all terms included in the model
+    selectInput("show.terms", "Coefficient to show", terms)
+  } else {
+    return(NULL)
+  }
+})
+
+
+# Calculate diagnostic measures
+influence.tab <- reactive({
+  if(!is.null( mod() )){
+    infls <- influence.measures(mod())    # Regression Deletion Diagnostics
+    infls <- data.frame(infls$infmat,
+      resid = residuals(mod()),
+      rstandard = rstandard(mod()),
+      rstudent = rstudent(mod()))
+    return(infls)
+  } else {
+    return(NULL)
+  }
+})
+
+
+# Order data according to influence measures
+new.datt <- reactive({
+  datt <- mod()$data      # extract data
+
+  if(input$infl.measure == "cov.r") { ii <- influence.tab()$cov.r }
+  if(input$infl.measure == "cook.d") { ii <- influence.tab()$cook.d }
+  if(input$infl.measure == "hat") { ii <- influence.tab()$hat }
+  if(input$infl.measure == "resid") { ii <- influence.tab()$resid }
+  if(input$infl.measure == "rstandard") { ii <- influence.tab()$rstandard }
+  if(input$infl.measure == "rstudent") { ii <- influence.tab()$rstudent }
+
+  datt <- datt[order(abs(ii), decreasing = TRUE), ]
+  return(datt)
+})
+
+
+
+build.model <- reactive({
+  if(input$family == "binomial")          { mm <- function(x){ glm(formulaText(), data=x, family=binomial()) }}
+  if(input$family == "gaussian")          { mm <- function(x){ glm(formulaText(), data=x, family=gaussian()) }}
+  if(input$family == "Gamma")             { mm <- function(x){ glm(formulaText(), data=x, family=Gamma()) }}
+  if(input$family == "inverse.gaussian")  { mm <- function(x){ glm(formulaText(), data=x, family=inverse.gaussian()) }}
+  if(input$family == "poisson")           { mm <- function(x){ glm(formulaText(), data=x, family=poisson()) }}
+  if(input$family == "quasibinomial")     { mm <- function(x){ glm(formulaText(), data=x, family=quasibinomial()) }}
+  if(input$family == "quasipoisson")      { mm <- function(x){ glm(formulaText(), data=x, family=quasipoisson()) }}
+  return(mm)
+})
+
+
+
+excl.coeffs <- reactive({
+  datt <- new.datt()       # get sorted data
+  fit <- build.model()     # get fitting function
+
+  coefss <- list()
+  coefss[[1]] <- data.frame(Obs = 0,       # initial model with all observations
+                  df.ci(confint(mod())),           # 95% CI for the coefficients
+                  Coef = coefficients(mod()))      # coefficients
+
+  for(i in 1:input$n.excl){         # start to exclude cases
+    tmp.mod <- try( fit( datt[-(1:i), ] ) )
+      if(any(class(tmp.mod) != "try-error")) { tmp.cc <- df.ci(confint(tmp.mod)) }
+      if(any(class(tmp.mod) == "try-error")) { tmp.cc <- data.frame(Variable=NA, LCI=NA, UCI=NA) }
+    coefss[[1+i]] <- data.frame(Obs = i, tmp.cc, Coef = coefficients(tmp.mod))
+    rm(tmp.mod, tmp.cc)
+  }
+
+  coefss <- do.call(rbind, coefss)
+  rownames(coefss) <- NULL
+
+  dropped.names <- data.frame(Number = 1:input$n.excl, Rowname = rownames(datt)[1:input$n.excl])
+  # new.names <- dropped.names$Rowname[match(coefss$obs, dropped.names$Rowname)]
+  terms <- names(coefficients(mod())) 
+  new.names <- c(
+    rep(0, length(terms)),                                                        # full model
+    as.numeric( as.character(rep(dropped.names$Rowname, each=length(terms)) ))    # with dropped terms
+    )
+  coefss <- data.frame(ObservationID = new.names, coefss)   # add real row IDs
+  return(coefss)
+})
+
+
+# output$eee <- renderPrint({ excl.coeffs() })
+
+
+output$plot.excl <- renderPlot({
+  if( is.null(input$show.terms) ) { return() }                 # nothing selected
+  if( !is.null(input$show.terms) ) {
+    ds <- subset(excl.coeffs(), Variable == input$show.terms)  # Extract chosen coefficient
+
+    y.lim <- pretty(  with(ds, c(Coef, LCI, UCI) ))
+
+    plot(1:nrow(ds), ds$Coef,
+         type="b", pch=16, lty=3,
+         ylab=ds$Variable[1], ylim=c(y.lim[1], y.lim[length(y.lim)]),
+         xlab="Excluded case ID", xaxt="n", las=2)
+    axis(1, at=1:nrow(ds), labels=ds$ObservationID)
+    len = .07                         # width of CI ticks
+    for (i in 1:nrow(ds)) {           # plot CI
+      arrows(i, ds$Coef[i],
+             i, ds$UCI[i],
+             angle=90, length=len)
+      arrows(i, ds$Coef[i],
+             i, ds$LCI[i],
+             angle=90, length=len)
+    }
+  }
+
+})
+
+
+
+# summary(fit)        # display results
+# confint(fit)        # 95% CI for the coefficients
+# exp(coef(fit))      # exponentiated coefficients
+# exp(confint(fit))   # 95% CI for exponentiated coefficients
+# predict(fit, type="response")     # predicted values
+# residuals(fit, type="deviance")   # residuals 
+
+
+
+
+# dbs_influential_obs <- which(apply(infls$is.inf, 1, any)) 
+# dbs_sans_influential_obs <- dbs1[-dbs_influential_obs,]
+
+
+
+######################################
+######################################
+######################################
+
+
+#debugging:  print all names and values in input  (based on Dmitry Grapov code)
+output$debug<- renderPrint({
+    # inptut variables:
+    obj <- names(input)
+    input.obj <- lapply(1:length(obj), function(i) { input[[obj[i]]]})
+    names(input.obj) <- obj
+
+    return(list(input = input.obj))
+           # , output = output.obj))
+})
+
+
+})
+
---
+++ ui.R
@@ -0,0 +1,112 @@
+## ui.r
+## The user interface controls the layout and appearance of the app.
+
+
+# Define UI for application
+shinyUI(pageWithSidebar(
+
+  # Application title
+  headerPanel(h3("GLM robustness")),
+
+
+  sidebarPanel(
+    fileInput("dataFile", "Upload file", accept="application/vnd.ms-excel"),
+    textInput("ff", "Model formula", ""),
+    helpText("Example:   y ~ x1 * x2"),
+    radioButtons("family", label = "Family:",
+          choices = list(
+            "binomial" = "binomial",
+            "gaussian" = "gaussian",
+            "Gamma" = "Gamma",
+            "inverse.gaussian" = "inverse.gaussian",
+            "quasibinomial" = "quasibinomial",
+            "quasipoisson" = "quasipoisson"),
+          selected = "gaussian"),
+    br(),
+    uiOutput("excl.n"),
+    br(),
+    radioButtons("infl.measure", "Regression diagnostics to use:",
+              list("Covariance ratio" = "cov.r",
+                   "Cook's distance" = "cook.d",
+                   "Diagonal elements of the hat matrix" = "hat",
+                   "Absolute residuals" = "resid",
+                   "Standardized residuals" = "rstandard",
+                   "Studentized residuals" = "rstudent"
+              ), selected = "cook.d"),
+    br(),
+    uiOutput("show"),
+    submitButton("Build model")
+  ),  # end of sidebarPanel
+
+
+  # Tabs with results
+  mainPanel(
+
+    tabsetPanel(
+
+      tabPanel("Full model results",
+        h5("Uploaded data structure:"),
+        verbatimTextOutput("data.descr"),
+        br(),
+        h5("Full model:"),
+        verbatimTextOutput("regTab"),
+        plotOutput("diagnost.1"),
+        plotOutput("diagnost.2"),
+        plotOutput("diagnost.3"),
+        plotOutput("diagnost.4")
+      ),
+
+      tabPanel("Influential effect",
+        # verbatimTextOutput("eee"),
+        plotOutput("plot.excl")
+      ),
+
+      tabPanel("Description",
+        h5("Typical Workflow"),
+        p("1. Upload your data: Tab-delimited text file with header (*.txt) or Excel file with one sheet (*.xls, *.xlsx)."),
+        p("2. Specify the model you would like to fit (see Formula interface for hints)."),
+        p("3. Check other settings (GLM family, maximum number of the most influential observations to exclude, criterion to estimate influence)."),
+        p("4. Press 'Build model' button - your model will be fitted to the full data set. You may check the results on the 'Full model results' tab."),
+        p("5. Go to the 'Influential effect' tab."),
+        p("6. On the sidebar choose which coefficients you would like to plot."),
+        p("7. Press 'Build model' again.  This step could be computer-intensive, so it may freeze for a while."),
+        p("That's it."),
+        br(),
+        h5("Formula interface"),
+        p("Here are several examples you may try (paste formula without quotes):"),
+        p("'y ~ A + B'   - Additive model with 2 predictors (no interaction)."),
+        p("'y ~ log(A) + sqrt(B)'   - The same but predictors are transformed."),
+        p("'y ~ A*B'   - Model with main effects and interaction = 'y ~ A + B + A:B'."),
+        p("'y ~ A*B*C'   - The same: main effects and interactions = 'y ~ A+B+C+A:B+A:C+B:C+A:B:C'."),
+        p("'y ~(A+B+C)^2' - A, B, and C crossed to level 2: 'y ~ A+B+C+A:B+A:C+B:C'."),
+        p("'y ~ A*B*C-A:B:C' - same as above: main effects plus 2-way interactions."),
+        p("Please use your 'real' varaiable names insted of 'y' or 'A' & 'B' from this examples."),
+        br(),
+        h5("Error distribution family - Model link function"),
+          p("binomial - logit"),
+          p("gaussian - identity"),
+          p("Gamma - inverse"),
+          p("inverse.gaussian - 1/mu^2"),
+          p("poisson - log"),
+          p("quasibinomial - logit"),
+          p("quasipoisson - log"),
+        br(),
+        h5("Notes"),
+        p("For GLMs (other than the Gaussian family with identity link) regression diagnostic measures are based on one-step approximations which may be inadequate if a case has high influence."),
+        br(),
+        br(),
+        br(),
+        br(),
+        br(),
+        br(),
+        p("Author - Vladimir Mikryukov, 04.07.2014")
+      ),
+
+      tabPanel("Debug",
+        h5("Debugging info:", style = "color:darkred"),
+        verbatimTextOutput("debug")         # return input and stored values
+      )
+
+    )     # close tabsetPanel
+  )       # close mainPanel
+))
